7bd7b96a0754376ea43a8a5e6615e0f4bf3d2137
noturavrigk <ksm92038386@gmail.com>
2026-01-16 11:56:54 +0900

    first commit

---
+++ src/test/java/goatHeaven/highLog/HighLogApplicationTests.java
@@ -0,0 +1,13 @@
+package goatHeaven.highLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
